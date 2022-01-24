9e6138b9e9bf59ec78c7544cc2efbb69972e698d
thisnotdefault <thisnotdefault@gmail.com>
2022-01-24 03:41:04 +0300

    add solution for lesson 2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # internet-shop
+
+First django project.
+
+<b>Install</b>
+
+<pre>pip install -r requirements.txt</pre>
+<pre>cd geekshop</pre>
+<pre>chmod +x run.sh</pre>
+
+<b>Launch</b>
+<pre>./run.sh</pre>
+
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,11 +1,69 @@
+import json
+
 from django.shortcuts import render
+from django.utils import timezone
+
+MENU_LINKS = [
+    {"url": "main", "name": "домой"},
+    {"url": "products", "name": "продукты"},
+    {"url": "contact", "name": "контакты"},
+]
+
 
 def main(reqest):
-    return render(reqest, 'mainapp/index.html')
+    return render(
+        reqest,
+        "mainapp/index.html",
+        context={
+            "current_year": timezone.now().year,
+            "title": "Главная",
+            "menu_links": MENU_LINKS,
+        },
+    )
+
 
 def products(reqest):
-    return render(reqest, 'mainapp/products.html')
+
+    # Вариант с импортом данных из файла
+    with open("./products.json", "r") as file:
+        products = json.load(file)
+
+    # Вариант простого списка
+    # products = [
+    #     {
+    #         'name': 'Люстра',
+    #         'description': 'Цветная люстра',
+    #         'image': 'img/product-11.jpg'
+    #     },
+    #     {
+    #         'name': 'Cтул',
+    #         'description': 'Удобный стул',
+    #         'image': 'img/product-21.jpg'
+    #     },
+    #     {
+    #         'name': 'Лампа',
+    #         'description': 'Уютная лампа',
+    #         'image': 'img/product-31.jpg'
+    #     },
+    # ]
+
+    return render(
+        reqest,
+        "mainapp/products.html",
+        context={
+            "title": "Продукты",
+            "menu_links": MENU_LINKS,
+            "products": products,
+        },
+    )
+
 
 def contact(reqest):
-    return render(reqest, 'mainapp/contact.html')
-
+    return render(
+        reqest,
+        "mainapp/contact.html",
+        context={
+            "title": "Контакты",
+            "menu_links": MENU_LINKS,
+        },
+    )
