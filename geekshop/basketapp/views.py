80423f3859be14ed4babf4b7c5d80a6023fd67a4
thisnotdefault <thisnotdefault@gmail.com>
2022-02-07 14:18:54 +0300

    add solution for lesson 6

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
@@ -6,12 +7,15 @@ from mainapp.models import Product
 from .models import Basket
 
 
+@login_required
 def view(request):
     return render(request, 'basketapp/basket.html', context={
-        'basket': Basket.objects.filter(user=request.user)
+        'title': 'Корзина',
+        
     })
 
 
+@login_required
 def add(request, product_id):
     product = get_object_or_404(Product,pk=product_id)
     basket_items = Basket.objects.filter(user=request.user, product=product)
@@ -26,8 +30,23 @@ def add(request, product_id):
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+
+@login_required
 def remove(request,basket_item_id):
     basket = get_object_or_404(Basket, pk=basket_item_id)
     basket.delete()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def edit(request, basket_item_id, quantity):
+    basket_item = Basket.objects.get(pk=basket_item_id)
+        
+    if quantity > 0:
+        basket_item.quantity = quantity
+        basket_item.save()
+    else:
+        basket_item.delete()
+    
+    return render(request, 'basketapp/includes/inc_basket_list.html')
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,6 @@
+import random
+
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
@@ -24,24 +27,30 @@ def index(reqest):
     )
 
 
+def get_hot_product(queryset):
+    return random.choice(queryset)
+
+
 def products(reqest):
-    catigories = ProductCategory.objects.all()
-    products = Product.objects.all()[:4]
+    categories = ProductCategory.objects.all()
+    products = Product.objects.all()
+    hot_product = get_hot_product(products)
     return render(
         reqest,
         "mainapp/products.html",
         context={
             "title": "Продукты",
             "menu_links": MENU_LINKS,
-            "products": products,
-            "catigories": catigories,
+            "products": products.exclude(pk=hot_product.pk)[:4],
+            "categories": categories,
+            "hot_product": hot_product,
         },
     )
 
 
-def category(reqest, pk):
-    catigories = ProductCategory.objects.all()
-    category = get_object_or_404(ProductCategory, pk=pk)
+def category(reqest, category_id):
+    categories = ProductCategory.objects.all()
+    category = get_object_or_404(ProductCategory, pk=category_id)
     products = Product.objects.filter(category=category)
     return render(
         reqest,
@@ -49,8 +58,24 @@ def category(reqest, pk):
         context={
             "title": "Продукты",
             "menu_links": MENU_LINKS,
-            "products": products,
-            "catigories": catigories,
+            "products": products[:4],
+            "categories": categories,
+            "hot_product": get_hot_product(products),
+        },
+    )
+
+
+def product(reqest, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    categories = ProductCategory.objects.all()
+    return render(
+        reqest,
+        "mainapp/product.html",
+        context={
+            "title": "Продукты",
+            "menu_links": MENU_LINKS,
+            "product": product,
+            "categories": categories,
         },
     )
 
